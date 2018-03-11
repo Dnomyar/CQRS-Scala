56180d47eab643fe9747c9ac416646b3c50cd37b
Damien RAYMOND <dam.ray49@gmail.com>
2018-03-11 21:15:51 +0100

    Add implementation of message bus

---
+++ src/main/scala/fr/damienraymond/cqrs/core/infrastructure/bus/MessageBus.scala
@@ -0,0 +1,82 @@
+package fr.damienraymond.cqrs.core.infrastructure.bus
+
+import com.google.inject.Inject
+import fr.damienraymond.cqrs.core.event.Event
+import fr.damienraymond.cqrs.core.middleware.{CommandMiddleware, Middleware}
+import fr.damienraymond.cqrs.core.{Command, CommandHandler, Handler, Message}
+
+import scala.language.higherKinds
+import scala.reflect.runtime.universe._
+
+
+/**
+  * TODO : A Query bus can have command
+  */
+trait MessageBus {
+
+  val middlewares: List[Middleware]
+
+  protected val classHandlerMap: Map[Type, Handler[Message[_], _]]
+
+  private val middlewareChain: Chain =
+    middlewares.foldRight[Chain](HandlerInvocation(classHandlerMap))(MiddlewareChainLink)
+
+
+  def dispatch[MESSAGE <: Message[RETURN_TYPE], RETURN_TYPE](message: MESSAGE)(implicit messageClass: Manifest[MESSAGE]): Either[String, (RETURN_TYPE, List[Event[_]])] =
+    middlewareChain.handleMiddlewareAndCallNext(message)
+
+
+
+  trait Chain {
+    def handleMiddlewareAndCallNext[RETURN_T](message: Message[RETURN_T]): Either[String, (RETURN_T, List[Event[_]])]
+  }
+
+  case class MiddlewareChainLink(current: Middleware, next: Chain) extends Chain {
+    override def handleMiddlewareAndCallNext[RETURN_T](message: Message[RETURN_T]): Either[String, (RETURN_T, List[Event[_]])] = {
+      println(s"Applying middleware : $current")
+      current.apply[RETURN_T](message, () => next.handleMiddlewareAndCallNext(message))
+    }
+  }
+
+  case class HandlerInvocation(handlers: Map[Type, Handler[Message[_], _]]) extends Chain {
+    override def handleMiddlewareAndCallNext[RETURN_T](message: Message[RETURN_T]): Either[String, (RETURN_T, List[Event[_]])] = {
+      handlers
+        .get(message.messageType) match {
+        case Some(handler) =>
+          Right(
+            handler
+              .asInstanceOf[Handler[Message[RETURN_T], RETURN_T]]
+              .handle(message))
+        case None => Left("No handler found") // TODO <-
+      }
+    }
+  }
+
+
+}
+
+
+//class QueryBus(override val middlewares: List[Middleware] = List.empty) extends MessageBus
+
+
+
+class CommandBusFactory @Inject()(handlers: List[Handler[Message[_],_]]) {
+
+  def create(middlewares: List[CommandMiddleware]): CommandBus = new CommandBus(handlers)(middlewares)
+
+}
+
+class CommandBus private[bus] (handlers: List[Handler[Message[_],_]])(override val middlewares: List[CommandMiddleware] = List.empty) extends MessageBus {
+
+  override protected val classHandlerMap: Map[Type, Handler[Message[_], _]] =
+    handlers.map{handler =>
+      val messageType: Type = handler.messageType
+      val h: Handler[Message[Any],Any] = handler.asInstanceOf[Handler[Message[Any],Any]]
+
+      val res: (Type, Handler[Message[_],_]) = messageType -> h
+      res
+    }.toMap
+
+}
+
+
